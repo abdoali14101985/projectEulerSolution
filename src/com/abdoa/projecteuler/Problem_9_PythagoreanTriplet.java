e96dd53cc898f7235a215f03e09ec17eba8640c4
Abdo Ali <abdoali.14101985@gmail.com>
2022-12-23 13:09:40 -0500

    Optimized Code for Problem 9 of ProjectEuler

--- src/com/abdoa/projecteuler/Problem_9_PythagoreanTriplet.java
+++ src/com/abdoa/projecteuler/Problem_9_PythagoreanTriplet.java
@@ -3,15 +3,14 @@ package com.abdoa.projecteuler;
 public class Problem_9_PythagoreanTriplet {
 
     public static void main(String[] args) {
-        for(int a = 1; a <1000; a++){
-            for (int b=1; b <1000; b++){
-                for (int c=1; c <1000; c++){
-                    if(a<b && b<c && (a*a + b*b) == (c * c) && a+b+c  == 1000){
-                        System.out.println(a+" "+ b + " "+ c);
-                        System.out.println(a*b*c);
-                        break;
-                    }
-
+        for(int a = 3; a <1000; a++){
+            for (int b=a+1; b <1000; b++){
+                double c = Math.pow(a,2) + Math.pow(b,2);
+                c = Math.sqrt(c);
+                if(a+b+c == 1000) {
+                    System.out.println(a + " " + b + " " + (int)c);
+                    System.out.println((long) (a * b * c));
+                    break;
                 }
             }
         }
